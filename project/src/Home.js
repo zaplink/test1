9b5c8b4be3612e0bb97de84e0c2590059e77988f
ThakshalaMadhuwanthi <hgtmadhuwanthi@gmail.com>
2024-10-24 10:36:06 +0530

    Create Home.js page

---
+++ project/src/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './home.css'
+
+export default function Home() {
+  return (
+    <div>
+        <body>
+        <h1>Go to Shopping</h1>
+        <div className="card-container">
+
+        <div class="card">
+           
+           <h2>Buy a purse</h2>
+           <p>Get a handmade purse</p>
+       </div>
+
+       <div class="card">
+          
+           <h2>Buy kitchen tools</h2>
+           <p>Tool set for kitchen</p>
+       </div>
+
+       <div class="card">
+           
+           <h2>Buy a lunch box</h2>
+           <p>Want to eat</p>
+       </div>
+
+        </div>
+      
+        </body>
+        
+    </div>
+  )
+}
