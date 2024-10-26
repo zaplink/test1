6b3cfa09029d2b1ea109d1438e47725b2d55d1ff
Sitharahansamali <hsithara69@gmail.com>
2024-10-27 03:27:17 +0530

    create summary

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Dining from "./pages/Dining"
 import Shopping from "./pages/Shopping"
 import Workout from "./pages/Workout";
+import Summary from "./pages/Summary";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/dining" element={<Dining/>} />
         <Route path="/shopping" element={<Shopping />} />
         <Route path="/Workout" element={<Workout/>} /> 
+        <Route path="/Summary" element={<Summary/>} /> 
       </Routes>
     </Router>
 
