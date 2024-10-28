8f5c4f92ea5c22633e5bfe83ea53a393a5e19144
u-sri2002 <usrimart@gmail.com>
2024-10-28 11:19:17 +0530

    built dining page

--- src/pages/Dining.tsx
+++ src/pages/Dining.tsx
@@ -1,17 +1,27 @@
-import { useNavigate } from "react-router-dom";
+import diningImage from '../img/dining.png';
 
 function Dining() {
-  const navigate = useNavigate();
-
-  function gotoShopping() {
-    navigate("/shopping");
-  }
   return (
-    <>
-      <p>This is Dining</p>
-      <button onClick={gotoShopping}>Go to shopping</button>
-    </>
+    <div className="container-dining">
+      <div className="left-column-dining">
+          <h1>Dining</h1>
+        <div className="content">
+          <ul>
+            <li>Cheese Cake</li>
+            <li>Rolls</li>
+            <li>Kirya</li>
+            <li>Mun Kewm</li>
+            <li>Cookies</li>
+          </ul>
+        </div>
+      </div>
+      <div className="right-column-dining">
+        <div className="img-div">
+          <img src={diningImage} alt="Dining table with food" />
+        </div>
+      </div>
+    </div>
   );
 }
-  
+
 export default Dining;
