83d78074c4fe85e0b62a2734979cdbc7eb55bff7
pathummadhusanka <pathum23200@gmail.com>
2024-10-31 13:06:33 +0530

    Rename shopping page CSS file

--- src/pages/Shopping.tsx
+++ src/pages/Shopping.tsx
@@ -1,44 +1,42 @@
-
-import '../ThakshalaNew.css';
+import "../shopping.css";
 const Shopping = () => {
-   
-  
-
-
-    return ( 
-    <div> 
+  return (
+    <div>
       <body>
-    <h1>Go to Shopping</h1>
+        <h1>Go to Shopping</h1>
 
-    <div className="card-container">
-        <div className="card">
-            <img src="https://via.placeholder.com/300x200?text=Purse" alt="Buy a purse" />
+        <div className="card-container">
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/300x200?text=Purse"
+              alt="Buy a purse"
+            />
             <h2>Buy a purse</h2>
             <p>Get a handmade purse</p>
-    
-        </div>
+          </div>
 
-        <div className="card">
-            <img src="https://via.placeholder.com/300x200?text=Kitchen+Tools" alt="Buy kitchen tools" />
-            
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/300x200?text=Kitchen+Tools"
+              alt="Buy kitchen tools"
+            />
+
             <h2>Buy kitchen tools</h2>
             <p>Tool set for kitchen</p>
-        </div>
+          </div>
 
-        <div className="card">
-            <img src="https://via.placeholder.com/300x200?text=Lunch+Box" alt="Buy a lunch box" />
+          <div className="card">
+            <img
+              src="https://via.placeholder.com/300x200?text=Lunch+Box"
+              alt="Buy a lunch box"
+            />
             <h2>Buy a lunch box</h2>
             <p>Want to eat</p>
+          </div>
         </div>
+      </body>
     </div>
-    </body>
-</div>
-
-
-);
-}
-
-
-
+  );
+};
 
 export default Shopping;
