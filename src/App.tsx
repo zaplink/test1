675fc2bd0d9a6564735bde75af8d2a7ee3a63871
chamodya <wichamodya@gmail.com>
2024-10-31 18:30:31 +0530

    add page 3 to development

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Dining from "./pages/Dining"
 import Shopping from "./pages/Shopping"
 import Workout from "./pages/Workout";
+import Summary from "./pages/Summary";
 
 function App() {
   return (
@@ -11,12 +12,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/dining" element={<Dining/>} />
         <Route path="/shopping" element={<Shopping />} />
-        <Route path="/Workout" element={<Workout/>} />
+        <Route path="/Workout" element={<Workout/>} /> 
+        <Route path="/Summary" element={<Summary/>} /> 
       </Routes>
     </Router>
 
 
   );
 }
-
 export default App;
--- src/pages/Workout.tsx
+++ src/pages/Workout.tsx
@@ -1,6 +1,56 @@
-export default function Workout(){
 
-    return(
-        <p>This is workout page</p>
-    );
+import '../workout.css';
+import gymPhoto from '/src/gym.avif';
+
+function Workout() {
+  return (
+    
+    <div className="App">
+      <h1>Go to Gym</h1>
+      <br></br>
+      <div className='photo'>
+        <img src={gymPhoto} style={{ width: '300px', height: '500px' }} />
+
+
+      </div>
+
+      <div className="exercise-section">
+        <h3>Bench Press (Barbell or Dumbbell):</h3>
+        <p>4 sets of 8-12 reps</p>
+      </div>
+
+      <div className="exercise-section">
+        <h3>Incline Dumbbell Press:</h3>
+        <p>4 sets of 8-12 reps</p>
+      </div>
+
+      <div className="exercise-section">
+        <h3>Chest Dips:</h3>
+        <p>3 sets of 8-12 reps</p>
+      </div>
+
+      <div className="exercise-section">
+        <h3>Cable Chest Flyers:</h3>
+        <p>4 sets of 10-12 reps</p>
+      </div>
+
+      <div className="exercise-section">
+        <h3>Tricep Dips:</h3>
+        <p>3 sets of 8-12 reps</p>
+      </div>
+
+      <div className="exercise-section">
+        <h3>Tricep Rope Pushdowns:</h3>
+        <p>3 sets of 10-12 reps</p>
+      </div>
+
+      {/* ... other exercises ... */}
+
+      <div className="button-section">
+        <button>Go Back to Agenda Page</button>
+      </div>
+    </div>
+  );
 }
+
+export default Workout;
